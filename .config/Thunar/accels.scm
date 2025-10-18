b6b58f86ff65fdef6558ed206a31545a17e7d769
Josie5734 <armyjoe999@gmail.com>
2025-10-18 19:52:37 +0100

    added custom .desktop entries

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -4,6 +4,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-last-modified" "")
 ; (gtk_accel_path "<Actions>/Thunarwindow/menu" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/d4dc0735f36e69cd5750be787143cbca" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/cut" "<Primary>x")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-size" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/file-menu" "")
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # dotfiles-puter
+
+***
+
+this is the dotfiles for my main pc
+
+Distro: Arch
+WM: Sway - with swaybg, swayidle, swaylock
+Bar: Waybar
+DMenu: Fuzzel
+Shell: ZSH (with oh-my-zsh)
+Terminal: Kitty
+File Explorer: Thunar
+Browser: Firefox
+
+***
+
+### notes
+
+***
+
+tuigreet is configured with the config file at `/etc/greetd/config.toml` which requires sudo to edit. I have left the `config.toml` in the `manual-configs` folder 
+
+
