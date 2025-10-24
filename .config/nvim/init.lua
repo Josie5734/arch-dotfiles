8b3a3e5d8ae7050ed5b83793eca2f25766bd8c76
Josie5734 <armyjoe999@gmail.com>
2025-10-24 20:09:48 +0100

    neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,4 +13,17 @@ vim.keymap.set('n', '<leader>w', ':w<CR>') --write
 vim.keymap.set('n', '<leader>q', ':q<CR>') --quit
 vim.keymap.set('n', '<leader>x', ':x<CR>') --save and quit
 
+vim.keymap.set('n', '<leader>e', ':Ex<CR>') --file explorer
+
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>') --write if changed, source file
+
+
+
+--plugins
+vim.pack.add({
+		{src = "https://github.com/vague-theme/vague.nvim"},
+})
+
+require("vague").setup({})
+
+vim.cmd("colorscheme vague")
