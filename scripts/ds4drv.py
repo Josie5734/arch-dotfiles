a579c481597d47a5755dc57b1bebe596176a086f
Josie5734 <armyjoe999@gmail.com>
2026-01-08 23:47:25 +0000

    idk thunar changed something

--- scripts/ds4drv.py
+++ scripts/ds4drv.py
@@ -3,19 +3,23 @@
 import os
 from pathlib import Path
 import subprocess
+import time
 
 # path to the ds4drv daemons pid file
-filepath = "/tmp/ds4drv.pid"
+pidfilepath = "/tmp/ds4drv.pid"
+
+# path to the ds4drv daemons log file
+logfilepath = "~/.cache/ds4drv.log"
 
 # check if the pid file exists
-if os.path.exists(filepath): # if True, then daemon is running
+if os.path.exists(pidfilepath): # if True, then daemon is running
 
     pid = None # get the pid of the daemon process
-    with open(filepath) as file:
+    with open(pidfilepath) as file:
         pid = file.read()
 
     subprocess.run(["kill",pid]) # kill the process
-    result = subprocess.run(["notify-send","'ds4drv daemon at {} killed'".format(pid)]) # send notification
+    subprocess.run(["notify-send","'ds4drv daemon at {} killed'".format(pid)]) # send notification
 
 else: # if False, daemon is not running
 
@@ -23,5 +27,5 @@ else: # if False, daemon is not running
     subprocess.run(["ds4drv","--daemon"])
 
     # output
-    with open(filepath) as file: 
+    with open(pidfilepath) as file: 
         subprocess.run(["notify-send","'ds4drv daemon started at {}'".format(file.read())])
