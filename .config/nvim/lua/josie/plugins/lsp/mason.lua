d1209e2faab0e6cb7e1a2935d7821651c2145f85
Josie5734 <armyjoe999@gmail.com>
2026-01-11 23:43:40 +0000

    added a new nvim config

---
+++ .config/nvim/lua/josie/plugins/lsp/mason.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+			},
+		},
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{ --formatters to install
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python lint
+			},
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
+}
