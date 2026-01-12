830a1ec3fdd026a18b18f7e18d2b249c70c1c998
Josie5734 <armyjoe999@gmail.com>
2026-01-12 19:50:40 +0000

    nvim config updates

--- .config/nvim/lua/josie/plugins/lsp/mason.lua
+++ .config/nvim/lua/josie/plugins/lsp/mason.lua
@@ -30,7 +30,6 @@ return {
 			ensure_installed = {
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"isort", -- python formatter
 				"black", -- python formatter
 				"pylint", -- python lint
 			},
